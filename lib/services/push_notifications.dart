5a71ddb7a581924599e071bc707aaecadb2cedc0
Fran72 <francisco_llorente@hotmail.com>
2023-11-27 14:29:15 +0100

    firebase messaging functions in service again

--- lib/services/push_notifications.dart
+++ lib/services/push_notifications.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
@@ -7,6 +9,23 @@ class PushNotifications {
   static FirebaseMessaging firebaseMessaging = FirebaseMessaging.instance;
   static String? token;
   static RemoteMessage? initialMessage;
+  static final StreamController<String> _messageStreamControllet =
+      StreamController.broadcast();
+
+  @pragma('vm:entry-point')
+  static Future _backgroundHandler(RemoteMessage message) async {
+    debugPrint('on background handles');
+  }
+
+  @pragma('vm:entry-point')
+  static Future _onMessageHandler(RemoteMessage message) async {
+    debugPrint('_onMessageHandler');
+  }
+
+  @pragma('vm:entry-point')
+  static Future _onMessageOpenApp(RemoteMessage message) async {
+    debugPrint('_onMessageOpenApp');
+  }
 
   @pragma('vm:entry-point')
   static Future initializeApp() async {
@@ -14,15 +33,13 @@ class PushNotifications {
         options: DefaultFirebaseOptions.currentPlatform);
     initialMessage = await FirebaseMessaging.instance.getInitialMessage();
     token = await FirebaseMessaging.instance.getToken();
-    debugPrint(token);
-    debugPrint('initial...........$initialMessage');
+
+    FirebaseMessaging.onBackgroundMessage(_backgroundHandler);
+    FirebaseMessaging.onMessage.listen(_onMessageHandler);
+    FirebaseMessaging.onMessageOpenedApp.listen(_onMessageOpenApp);
+  }
+
+  static closeStreams() {
+    _messageStreamControllet.close();
   }
 }
-
-
-// SHA1: 1F:94:33:D7:86:C8:37:65:4B:C0:45:9D:76:3A:8F:A1:B3:BD:0F:F8
-// SHA-256: CF:53:7B:A6:26:26:9E:01:5E:2D:BC:2E:18:01:1B:0D:8B:39:C1:7A:AC:2C:5D:EB:37:AD:02:EF:AF:EB:C3:64
-
-// token: fHI9R373QsiAM7pXzXCqJc:APA91bHPcxNFwb7QjQUrwuYVa1gmpIWmbybk4rjAtHtjP83VWhORUZGDs7en0aZ5DnqnzQamWUt6y5Nd1BL8yGu8AVBDQLVRYq1YxJjp8MwNI2a8WBU5o5qn1kcak6gcCj3PhADtmeeD
-
-// gitignore test 2b
